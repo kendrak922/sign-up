5c4626c971bf159843202557bfc58c5aa6864e04
Kendra Kyndberg <kendrak922@gmail.com>
2020-07-05 20:11:44 -0500

    fixed modal

--- app.js
+++ app.js
@@ -22,25 +22,59 @@ $(document).ready(function () {
     count = snapshot.val().firstShift
     $('#clickValue').html(count);
   })
+ //append button
+ $('.shifts-available').append('<a class="waves-effect btn modal-trigger" data-target="modal1" id="signup1">Sign Up</a>')
+
   //subtract
-    $("#signup1").on('click', function () {
-      if(count > 0){
+  $("#signup1").on('click', function () {
+    if (count > 0) {
       count--
       firebase.database().ref().set({
         firstShift: count
       })
-      return count; 
-    }else{
-      M.toast({html: 'No Availble Shifts'})
+      return count;
+    } else {
+      $(this).removeClass('modal-trigger')
+      M.toast({ html: 'No Availble Shifts' })
     }
-        });
+  });
+ 
+ 
+  //moment
+  moment().format();
 
-//moment
-moment().format();
+  const day = moment().format('MMMM Do YYYY');
+  $("#today").html(day);
 
-const day = moment().format('MMMM Do YYYY');
-$("#today").html(day);
+  //add volunteer shift to database
+
+let database = firebase.database();
+
+  let name= '';
+  let phone = '';
+  let email = '';
+  let shift = ''
+  let date = '';
 
 
+  $(".submit").on('click', function(event){
+  event.preventDefault();
+
+    name= $("#name").val().trim();
+    phone= $("#phone").val().trim();
+    email= $("#email").val().trim();
+
+    let volunteer ={
+      Name: name,
+      Phone: phone,
+      Email : email,
+    };
+    database.ref().push(volunteer)
+
+    $('#name').val('');
+    $('#phone').val('');
+    $('#email').val('');
+
+  })
 
 });
